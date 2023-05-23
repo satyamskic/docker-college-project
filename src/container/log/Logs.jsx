27b5f4d6e247426463dff1149894704022249e14
satyamskic <satyamskic807681@gmail.com>
2023-05-23 16:45:36 +0530

    centralized download log button

--- src/container/log/Logs.jsx
+++ src/container/log/Logs.jsx
@@ -139,7 +139,7 @@ function Logs(props) {
       {isLoading ? <Backdrop open>
         <CircularProgress color="inherit" />
       </Backdrop> : isLoading}
-      <button onClick={downloadHtml} class="btn"><i class="fa fa-download"></i></button>
+      <center ><button onClick={downloadHtml} class="btn"><i class="fa fa-download"></i></button></center>
       <div class="scroll-bg">
         <div className="scroll-div">
           <div className="scroll-object">
